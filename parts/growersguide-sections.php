f12096efb7ccdceb553518fe7e57374a0c3916b3
tombola <tombola@github>
2024-11-19 17:05:37 +0000

    Allow growers guide heading tag to be configured

--- parts/growersguide-sections.php
+++ parts/growersguide-sections.php
@@ -3,6 +3,9 @@
 // rather than the growing guide itself
 $ggid = $args["growing_guide_id"] ?? false;
 
+// Set the heading tag from args or default to h2 for the main growing guide
+$htag = $args["heading_tag"] ?? (!$ggid ? 'h2' : 'h3');
+
 if ($hero = get_field('hero', $ggid)) {
 ?>
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
@@ -24,30 +27,30 @@ if ($hero = get_field('hero', $ggid)) {
         }
 
         if ($seed_sowing = get_field('seed_sowing', $ggid)) {
-            echo "<h2 id='sow'>Seed Sowing</h2>";
+            echo "<$htag id='sow'>Seed Sowing</$htag>";
             echo $seed_sowing;
         }
         if ($transplanting = get_field('transplanting', $ggid)) {
-            echo "<h2 id='transplant'>Transplanting</h2>";
+            echo "<$htag id='transplant'>Transplanting</$htag>";
             echo $transplanting;
         }
         if ($plant_care = get_field('plant_care', $ggid)) {
-            echo "<h2 id='care'>Plant Care</h2>";
+            echo "<$htag id='care'>Plant Care</$htag>";
             echo $plant_care;
         }
         if ($challenges = get_field('challenges', $ggid)) {
-            echo "<h2 id='challenges'>Challenges</h2>";
+            echo "<$htag id='challenges'>Challenges</$htag>";
             echo $challenges;
         }
         if ($harvest = get_field('harvest', $ggid)) {
-            echo "<h2 id='harvest'>Harvest</h2>";
+            echo "<$htag id='harvest'>Harvest</$htag>";
             echo $harvest;
         }
         if ($culinary_ideas = get_field('culinary_ideas', $ggid)) {
-            echo "<h2 id='culinary'>Culinary Ideas</h2>";
+            echo "<$htag id='culinary'>Culinary Ideas</$htag>";
             echo $culinary_ideas;
         }
         if ($seed_saving = get_field('seed_saving', $ggid)) {
-            echo "<h2 id='seeds'>Seed Saving</h2>";
+            echo "<$htag id='seeds'>Seed Saving</$htag>";
             echo $seed_saving;
         }
