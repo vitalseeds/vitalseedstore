4837fc9dfff042bb86871f200b977f22f467340d
tombola <tombola@github>
2025-03-10 09:26:01 +0000

    Style invoice shipping vat

--- functions.php
+++ functions.php
@@ -228,7 +228,7 @@ add_filter('woocommerce_get_breadcrumb', 'vital_remove_seeds_from_breadcrumbs', 
 /**
  * Adds VAT information to the shipping total in the order invoice.
  *
- * For the other modifications to the invoice, see template at woocommerce/pdf/vital_invoice/invoice.php
+ * For the other modifications to the invoice, see template at 	woocommerce/pdf/vital_invoice/invoice.php
  *
  * This function modifies the order item totals to include VAT information
  * in the shipping total if there is shipping tax applied to the order.
@@ -241,7 +241,7 @@ function vital_invoice_order_totals($order_item_totals, $order) {
 	if ($order->get_shipping_tax() > 0) {
 		$order_item_totals['shipping'] = array(
 			'label' => $order_item_totals['shipping']['label'],
-			'value' => $order_item_totals['shipping']['value'] . " (includes " . wc_price( $order->get_shipping_tax(), array( 'currency' => $order->get_currency() ) ) . ' VAT)',);
+			'value' => $order_item_totals['shipping']['value'] . "<small> (includes " . wc_price( $order->get_shipping_tax(), array( 'currency' => $order->get_currency() ) ) . ' VAT)</small>',);
 	}
 	return $order_item_totals;
 }
