085e33e06cf17aa4a6f73231e0aefa9115aee7e8
tombola <tombola@github>
2025-08-06 13:49:01 +0100

    Fix storefront css load order

--- functions.php
+++ functions.php
@@ -6,12 +6,11 @@ function my_theme_enqueue_styles()
 {
 
 	$parent_style = 'storefront-style';
-
-	wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+	// wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
 	wp_enqueue_style(
 		'child-style',
 		get_stylesheet_directory_uri() . '/style.css',
-		array($parent_style),
+		array($parent_style), // Ensure storefront is loaded first
 		wp_get_theme()->get('Version')
 	);
 }
